60ead39acfbc8e80e248792c00f46fef4ea6d56f
Sarah Paz <sarah.paz@gmail.com>
2018-12-21 01:07:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  i = 0
+  while i < array do |item|
+    return item
+    yield
+  end
 end
+
+my_each("hello", "hi", "bye")
