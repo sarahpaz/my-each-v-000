a069583986c66adf053a926c0e88e2f9b6b730c2
Sarah Paz <sarah.paz@gmail.com>
2018-12-21 01:21:02 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,9 @@
 def my_each(array) # put argument(s) here
   # code here
   i = 0
-  while i < array do |item|
-    return item
-    yield
+  while i < array.length
+    yield array[i]
+    i += 1
   end
 end
 
-my_each("hello", "hi", "bye")
